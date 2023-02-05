b2b4dc8256f2638cb03504c68ea8f335b4e9eb14
Erasthos <erasthos.firestorm@gmail.com>
2023-02-05 04:44:40 -0300

    Agregar archivos de proyecto.

---
+++ GmHelper/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace GmHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.selectFolderButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(12, 10);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(143, 20);
+            this.searchTextBox.TabIndex = 0;
+            this.searchTextBox.Click += new System.EventHandler(this.searchTextBox_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(71, 36);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(161, 8);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "button1";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // selectFolderButton
+            // 
+            this.selectFolderButton.Location = new System.Drawing.Point(12, 59);
+            this.selectFolderButton.Name = "selectFolderButton";
+            this.selectFolderButton.Size = new System.Drawing.Size(224, 23);
+            this.selectFolderButton.TabIndex = 3;
+            this.selectFolderButton.Text = "button2";
+            this.selectFolderButton.UseVisualStyleBackColor = true;
+            this.selectFolderButton.Click += new System.EventHandler(this.selectFolderButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 97);
+            this.Controls.Add(this.selectFolderButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.searchTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.2D;
+            this.Text = "GmHelper";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button selectFolderButton;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ GmHelper/Form1.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GmHelper
+{
+    public partial class Form1 : Form
+    {
+        private string folderPath;
+        private string Text1 = "Seleccione la carpeta donde se encuentran sus macros.";
+        private string Text2 = "Configuración.";
+        private string Text3 = "Se encontraron coincidencias en los siguientes archivos:\n\n";
+        private string Text4 = "Resultados de la búsqueda";
+        private string Text5 = "No se encontraron coincidencias.";
+        private string Text6 = "ID misión";
+        private string Text7 = "Buscar en WowHead";
+        private string Text8 = "Buscar";
+        private string Text9 = "Carpeta Macros";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            searchTextBox.Text = Text6;
+            checkBox1.Text = Text7;
+            searchButton.Text = Text8;
+            selectFolderButton.Text = Text9;
+
+            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\GmHelper");
+
+            if (registryKey != null)
+            {
+                folderPath = (string)registryKey.GetValue("folderPath");
+                registryKey.Close();
+            }
+
+            else
+            {
+                MessageBox.Show(Text1, Text2);
+                selectFolderButton_Click(null, EventArgs.Empty);
+            }
+
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            switch (RectangleToScreen(Bounds).Contains(PointToScreen(Cursor.Position)))
+            {
+                case true:
+                    if (Opacity >= 1) { }
+                    else
+                    {
+                        Opacity += 0.20;
+
+                    }
+                    break;
+                case false:
+                    if (Opacity >= 0.21)
+                    {
+                        Opacity -= 0.20;
+
+                    }
+                    break;
+            }
+        }
+
+        private void searchTextBox_Click(object sender, EventArgs e)
+        {
+            searchTextBox.Text = String.Empty;
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string searchTerm = searchTextBox.Text;
+            string[] files = Directory.GetFiles(folderPath);
+            string resultFiles = "";
+
+            foreach (string file in files)
+            {
+                string fileText = File.ReadAllText(file);
+                if (fileText.Contains(searchTerm))
+                {
+                    resultFiles += Path.GetFileName(file) + "\n";
+                }
+            }
+
+            if (resultFiles.Length > 0)
+            {
+                MessageBox.Show(Text3 + resultFiles, Text4);
+                if (checkBox1.Checked == true)
+                {
+                    string wowheadLink = "https://wowhead.com/es/quest=" + searchTerm;
+                    Process.Start(wowheadLink);
+                }
+            }
+            else
+            {
+                MessageBox.Show(Text5, Text4);
+            }
+        }
+
+        private void selectFolderButton_Click(object sender, EventArgs e)
+        {
+            using (var folderBrowserDialog = new FolderBrowserDialog())
+            {
+                DialogResult result = folderBrowserDialog.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+                {
+                    string selectedFolderPath = folderBrowserDialog.SelectedPath;
+                    folderPath = selectedFolderPath;
+
+                    RegistryKey registryKey = Registry.CurrentUser.CreateSubKey("SOFTWARE\\GmHelper");
+                    registryKey.SetValue("folderPath", folderPath);
+                    registryKey.Close();
+                }
+                else
+                {
+                    selectFolderButton_Click(null, EventArgs.Empty);
+                }
+            }
+        }
+    }
+}
